4ec0ac561b226849aa287968792122ffc323f23b
Thuc Nguyen <johnthuc1997@gmail.com>
2021-04-29 10:12:37 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const apiSongs = 'https://my-json-server.typicode.com/JohnThuc031097/api_music_player/songs';
+
+/**
+ * Format App
+ * 1. Render Song
+ */
